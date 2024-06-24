8596e417d0d8d92b5d677b957524b4b70ec3cce9
guilhermeguedes <guilhermeguedes.gag@gmail.com>
2024-06-24 09:03:46 -0300

    Video 2.3 inicio

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,11 +1,20 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+
+    @SerializedName("Year")
     private int anoDeLancamento;
+
     private boolean incluidoNoPlano;
+
     private double somaDasAvaliacoes;
+
     private int totalDeAvaliacoes;
+
     private int duracaoEmMinutos;
 
     public Titulo(String nome, int anoDeLancamento) {
@@ -66,5 +75,11 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento ;
     }
 }
--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -8,12 +8,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=767e22b0";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=767e22b0"))
+                .uri(URI.create(endereco))  //you can also put address here
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
@@ -27,7 +37,8 @@ public class PrincipalComBusca {
 
         Gson gson = new Gson();
         Titulo meuTitulo = gson.fromJson(json, Titulo.class);
-    System.out.println("Titulo:" + meuTitulo.getNome());
+//        System.out.println("Titulo:" + meuTitulo.getNome());
+        System.out.println(meuTitulo);
 
     }
 }
