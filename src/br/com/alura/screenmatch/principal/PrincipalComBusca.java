b4042090921920a0129790f46388aa589f505c60
guilhermeguedes <guilhermeguedes.gag@gmail.com>
2024-06-24 00:40:35 -0300

    Video 2.2

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,14 +1,33 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class PrincipalComBusca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://foo.com/"))
+                .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=767e22b0"))
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json =response.body();
+        // Response body será o cara que imprimir o json, ou seja, serializar
+        //TODO: salve this to Git Book
+
+
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+    System.out.println("Titulo:" + meuTitulo.getNome());
+
     }
 }
