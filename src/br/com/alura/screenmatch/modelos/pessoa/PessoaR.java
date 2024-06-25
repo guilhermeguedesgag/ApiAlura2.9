e71ce94d774d060ea5ab51842e746be1231cf0b2
guilhermeguedes <guilhermeguedes.gag@gmail.com>
2024-06-25 01:14:44 -0300

    Começando o projeto novo com o record

--- src/br/com/alura/screenmatch/modelos/pessoa/PessoaR.java
+++ src/br/com/alura/screenmatch/modelos/pessoa/PessoaR.java
@@ -1,6 +1,31 @@
 package br.com.alura.screenmatch.modelos.pessoa;
 
     public record PessoaR(String nome, String idade, String profissao, String cidade) {
-
+       // aqui ele gravou a estrutura do Record, a pessoa terá está estrutura
     }
+    /*Todo: esse record acima, resumiu todo esse conteudo abaixo:
+
+        public class Pessoa {
+        private String nome;
+        private String idade;
+        private String profissao;
+        private String cidade;
+
+
+
+        public Pessoa(String nome, String idade, String profissao, String cidade) {
+            this.nome = nome;
+            this.idade = idade;
+            this.profissao = profissao;
+            this.cidade = cidade;
+        }
+        @Override
+        public String toString() {
+            return "Pessoa{" +
+                    "nome='" + nome + '\'' +
+                    ", idade='" + idade + '\'' +
+                    ", profissão='"  + profissao + '\'' + ", cidade='" + cidade + '\'' + '}';
+        }
+
+     */
 
